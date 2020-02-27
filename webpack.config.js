1ac1f5294d368f248ff3ee4ab6453529b8d6017d
Matheus Goncalves Murta <matgoncalv@engdb.infra>
2020-02-27 17:55:59 -0300

    html loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
-const glob = require('glob')
+const glob = require('glob');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const PurgecssPlugin = require('purgecss-webpack-plugin')
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 const PATHS = {
     src: path.join(__dirname, 'src')
@@ -9,7 +11,6 @@ const PATHS = {
 
 module.exports = {
   entry: "./src/index.js",
-
   output: {
     filename: "main.js",
     path: path.resolve("dist")
@@ -17,6 +18,10 @@ module.exports = {
 
   module: {
     rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -46,5 +51,15 @@ module.exports = {
     new PurgecssPlugin({
         paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
     }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html'
+
+    }),
+    new HtmlWebpackPlugin({
+      template: 'about.html',
+      filename: 'about.html'
+    })
+    
   ]
 };
